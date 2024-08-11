fce1a9341d7fc39c29bbd7b362081797d19a498f
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-11 23:39:34 +0530

    solution

---
+++ Easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) { 
+      if (head == null || head.next == null) {
+          return head;
+      }
+      
+      // Initialize the current node to the head of the list
+      ListNode current = head;
+      
+      // Traverse the list
+      while (current != null && current.next != null) { 
+          if (current.val == current.next.val) { 
+              current.next = current.next.next;
+          } else { 
+              current = current.next;
+          }
+      } 
+      return head;
+  }
+ public static void main(String[] args) {
+    
+ }   
+}
